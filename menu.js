18f9deff96252fdbf12dd8adf3fe7b801db4d1a1
Alberte <alje0002@stud.kea.dk>
2025-06-09 18:31:49 +0200

    tilføjet nyt efter fejl

---
+++ menu.js
@@ -0,0 +1,8 @@
+// fundet inspiration fra w3 schools https://www.w3schools.com/howto/howto_js_curtain_menu.asp
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
